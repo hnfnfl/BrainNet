78ab32ab1f009bb60ba889b1534c16942ec86014
hnfnfl <kakaknaufal@gmail.com>
2023-04-14 07:46:54 +0700

    ongoing: - work from 14/04/23 (add multi marker)

--- Korem/src/main/java/com/jaylangkung/korem/dataClass/GiatData.kt
+++ Korem/src/main/java/com/jaylangkung/korem/dataClass/GiatData.kt
@@ -6,7 +6,13 @@ data class GiatData(
     val keterangan: String,
     val lokasi: String,
     val mulai: String,
+    val posisi_giat: ArrayList<GiatPosisiGiat>,
     val proses: String,
     val sampai: String,
     val tujuan: String
+)
+
+data class GiatPosisiGiat(
+    val lat: String,
+    val lng: String
 )
--- Korem/src/main/java/com/jaylangkung/korem/giat/GiatActivity.kt
+++ Korem/src/main/java/com/jaylangkung/korem/giat/GiatActivity.kt
@@ -1,16 +1,22 @@
 package com.jaylangkung.korem.giat
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
-import android.view.View
 import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import androidx.recyclerview.widget.DefaultItemAnimator
-import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.jaylangkung.korem.MainActivity
 import com.jaylangkung.korem.R
 import com.jaylangkung.korem.dataClass.GiatResponse
@@ -20,15 +26,20 @@ import com.jaylangkung.korem.retrofit.RetrofitClient
 import com.jaylangkung.korem.utils.Constants
 import com.jaylangkung.korem.utils.ErrorHandler
 import com.jaylangkung.korem.utils.MySharedPreferences
+import es.dmoral.toasty.Toasty
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class GiatActivity : AppCompatActivity() {
+class GiatActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var binding: ActivityGiatBinding
     private lateinit var myPreferences: MySharedPreferences
     private lateinit var adapter: GiatAdapter
+
+    private lateinit var client: FusedLocationProviderClient
+    private lateinit var mMap: GoogleMap
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -56,6 +67,10 @@ class GiatActivity : AppCompatActivity() {
             }
         })
 
+        client = LocationServices.getFusedLocationProviderClient(this@GiatActivity)
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.giat_all_map) as SupportMapFragment
+        mapFragment.getMapAsync(this@GiatActivity)
+
         val iduser = myPreferences.getValue(Constants.USER_IDAKTIVASI).toString()
         val tokenAuth = getString(R.string.token_auth, myPreferences.getValue(Constants.TokenAuth).toString())
 
@@ -72,40 +87,71 @@ class GiatActivity : AppCompatActivity() {
         }
     }
 
+    @SuppressLint("PotentialBehaviorOverride")
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        //permission access fine & coarse location
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            Toasty.error(this, "Mohon izinkan lokasi pada aplikasi", Toasty.LENGTH_LONG).show()
+            onBackPressedDispatcher.onBackPressed()
+            return
+        }
+//        client.lastLocation.addOnCompleteListener {
+//            Priority.PRIORITY_HIGH_ACCURACY
+//            val userLat = it.result.latitude
+//            val userLong = it.result.longitude
+//            val pos = LatLng(userLat, userLong)
+//            Log.e("posisi user", "$userLat, $userLong")
+//            drawMarker(pos)
+//        }
+    }
+
     private fun getGiat(iduser_aktivasi: String, tokenAuth: String) {
         val service = RetrofitClient().apiRequest().create(DataService::class.java)
         service.getGiat(iduser_aktivasi, tokenAuth).enqueue(object : Callback<GiatResponse> {
             override fun onResponse(call: Call<GiatResponse>, response: Response<GiatResponse>) {
                 if (response.isSuccessful) {
                     if (response.body()!!.status == "success") {
-                        binding.loadingAnim.visibility = View.GONE
                         val listData = response.body()!!.data
-                        adapter.setItem(listData)
-                        adapter.notifyItemRangeChanged(0, listData.size)
-
-                        with(binding.rvGiatList) {
-                            layoutManager = LinearLayoutManager(this@GiatActivity)
-                            itemAnimator = DefaultItemAnimator()
-                            setHasFixedSize(true)
-                            adapter = this@GiatActivity.adapter
+                        for (data in listData) {
+                            val lat = data.posisi_giat[0].lat.toDouble()
+                            val lng = data.posisi_giat[0].lng.toDouble()
+                            val giatPos = LatLng(lat, lng)
+                            mMap.setOnMapLoadedCallback {
+                                drawMarker(data.tujuan, giatPos)
+                            }
                         }
                     }
-                } else {
-                    binding.loadingAnim.visibility = View.GONE
-                    ErrorHandler().responseHandler(
-                        this@GiatActivity,
-                        "getGiat | onResponse", response.message()
-                    )
                 }
             }
 
             override fun onFailure(call: Call<GiatResponse>, t: Throwable) {
-                binding.loadingAnim.visibility = View.GONE
                 ErrorHandler().responseHandler(
                     this@GiatActivity,
                     "getGiat | onFailure", t.message.toString()
                 )
             }
         })
+    }
+
+    private fun drawMarker(title: String, pos: LatLng) {
+        val markerOptions = MarkerOptions()
+            .position(pos)
+            .title(title)
+            .draggable(true)
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(pos, 18f))
+        mMap.uiSettings.isScrollGesturesEnabled = true
+        mMap.uiSettings.isZoomGesturesEnabled = true
+        mMap.mapType = GoogleMap.MAP_TYPE_NORMAL
+        val show = mMap.addMarker(markerOptions)
+        show?.showInfoWindow()
     }
 }
